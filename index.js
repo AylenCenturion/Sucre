14989c4fc5db4feaa98141d90590528b91481b96
Aylen Centurion <aylenlcenturion@gmail.com>
2023-02-02 19:20:44 -0300

    js restructured

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const cartMenu = document.querySelector(".cart");
-const cartBtn = document.querySelector(".cartLogo")
-const backBlur = document.querySelector(".blur")
-const userMenu = document.querySelector(".userNav")
-const userBtn = document.querySelector(".userLogo")
-const navMenu = document.querySelector(".navMenu")
-const burgerLogo = document.querySelector(".burgerLogo")
-const closeCartBtn = document.querySelector(".close-cart")
-const buyBtn = document.querySelector(".buyBtn")
-const deleteBtn = document.querySelector(".deleteBtn")
-const products = document.querySelector(".allProducts")
-const popularProducts = document.querySelector(".popularProducts")
-const productsCart = document.querySelector(".shoppingList")
-const cartCount = document.querySelector(".cartCount")
-const cartBottom = document.querySelector(".cartBottom")
-const total = document.querySelector(".total")
-const subtotal = document.querySelector(".subtotal")
-const categories = document.querySelector(".categories")
-const categoriesList = document.querySelectorAll(".category")
-const moreBtn = document.querySelector(".moreBtn")
-const addBtn = document.querySelector(".addBtn")
-const successModal = document.querySelector(".successModal")
+const cartBtn = document.querySelector(".cartLogo");
+const backBlur = document.querySelector(".blur");
+const userMenu = document.querySelector(".userNav");
+const userBtn = document.querySelector(".userLogo");
+const navMenu = document.querySelector(".navMenu");
+const burgerLogo = document.querySelector(".burgerLogo");
+const closeCartBtn = document.querySelector(".close-cart");
+const buyBtn = document.querySelector(".buyBtn");
+const deleteBtn = document.querySelector(".deleteBtn");
+const productsCart = document.querySelector(".shoppingList");
+const cartCount = document.querySelector(".cartCount");
+const cartBottom = document.querySelector(".cartBottom");
+const total = document.querySelector(".total");
+const subtotal = document.querySelector(".subtotal");
 
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
@@ -52,103 +45,8 @@ const closeCart = () => {
 }
 
 const logInAlert = () => {
-    if(!cart.length) return
+    if(users.length) return
   alert("Please log in to complete the purchase");
-}
-
-//page products
-
-const renderProduct = (product) => {
-    const {id, name, price, img} = product;
-
-    return`
-    <div class="productItem">
-      <img src="${img}" alt=${name}>
-      <div class="productData">
-        <p class="productName">${name}</p>
-        <p class="productPrice">$${price}</p>
-        <button class="cartBtn addBtn" 
-          data-id='${id}' 
-          data-name='${name}'
-          data-price='${price}'
-          data-img='${img}'
-          >Add to cart</button>
-      </div>
-    </div>
-    `
-}
-
-const renderPopulars = () => {
-    const popularList = productsData.filter(
-        product => product.popular === true
-    )
-    popularProducts.innerHTML = popularList.map(renderProduct).join('')
-}
-
-const renderDividedProducts = (index = 0) => {
-    products.innerHTML += productsController.dividedProducts[index].map(renderProduct).join('');
-}
-
-const renderFilteredProducts = (category) => {
-    const productsList = productsData.filter(
-        (product) => product.category === category
-    )
-    products.innerHTML = productsList.map(renderProduct).join('')
-}
-
-const renderProducts = (index = 0, category = undefined) =>{
-    if(!category) {
-        renderDividedProducts(index);
-        return;
-    }
-    renderFilteredProducts(category)
-}
-
-const changeCategory = (selectedCategory) => {
-    const categories = [...categoriesList];
-    categories.forEach((categoryBtn) => {
-        if(categoryBtn.dataset.category !== selectedCategory){
-            categoryBtn.classList.remove('categoryActive');
-            return;
-        }
-        categoryBtn.classList.add('categoryActive');
-    })
-}
-
-const toggleMoreBtn = (category) => {
-    if (!category) {
-        moreBtn.classList.remove('hidden');
-        return
-    }
-    moreBtn.classList.add('hidden');
-};
-
-const changeFilterState = (e) => {
-    const selectedCategory = e.target.dataset.category;
-    changeCategory(selectedCategory);
-    toggleMoreBtn(selectedCategory);
-}
-
-const applyFilter = (e) => {
-    if (!e.target.classList.contains('category')) return;
-    changeFilterState(e);
-    if(!e.target.dataset.category){
-        products.innerHTML = '';
-        renderProducts();
-    }else{
-        renderProducts(0, e.target.dataset.category);
-        productsController.nextProductsIndex =1;
-    }
-}
-
-const isLastIndexOf = () => productsController.nextProductsIndex === productsController.productsLimit;
-
-const showMoreProducts =() => {
-    renderProducts(productsController.nextProductsIndex);
-    productsController.nextProductsIndex ++;
-    if (isLastIndexOf()){
-        moreBtn.classList.add('hidden');
-    }
 }
 
 //cart function
@@ -232,29 +130,6 @@ const checkCartState = () => {
     disabledBtn(deleteBtn)
 }
 
-const showSuccessModal = (msg) => {
-    successModal.classList.add('activeModal');
-    successModal.textContent = msg;
-    setTimeout (() => {
-        successModal.classList.remove('activeModal');
-    }, 1600)
-}
-
-const addProducts = (e) => {
-    if(!e.target.classList.contains('addBtn')) return;
-    const {id, name, price, img} = e.target.dataset;
-    const product = productData (id, name, price, img);
-    if (isExistingProduct(product)) {
-        addUnitToProduct(product);
-        showSuccessModal ("Unit added successfully");
-
-    } else {
-        createCartProduct (product);
-        showSuccessModal ("Product added successfully");
-    }
-    checkCartState ()
-}
-
 const removeProductFromCart = (existingProduct) => {
     cart = cart.filter(product => product.id !== existingProduct.id)
     checkCartState()
@@ -308,14 +183,8 @@ const init = () => {
     backBlur.addEventListener("click", closeCart);
     closeCartBtn.addEventListener("click", closeCart)
     buyBtn.addEventListener("click", logInAlert)
-    renderProducts()
-    categories.addEventListener("click", applyFilter)
-    moreBtn.addEventListener("click", showMoreProducts)
-    renderPopulars()
     window.addEventListener("load", renderCart)
     window.addEventListener("load", showTotal)
-    products.addEventListener("click", addProducts)
-    popularProducts.addEventListener("click", addProducts)
     productsCart.addEventListener("click", handlerQuantity)
     deleteBtn.addEventListener("click", deleteCart);
     disabledBtn(buyBtn)
---
+++ products.js
@@ -0,0 +1,138 @@
+const products = document.querySelector(".allProducts");
+const popularProducts = document.querySelector(".popularProducts");
+const categories = document.querySelector(".categories");
+const categoriesList = document.querySelectorAll(".category");
+const moreBtn = document.querySelector(".moreBtn");
+const addBtn = document.querySelector(".addBtn");
+const successModal = document.querySelector(".successModal");
+
+const renderProduct = (product) => {
+  const {id, name, price, img} = product;
+
+  return`
+  <div class="productItem">
+    <img src="${img}" alt=${name}>
+    <div class="productData">
+      <p class="productName">${name}</p>
+      <p class="productPrice">$${price}</p>
+      <button class="cartBtn addBtn" 
+        data-id='${id}' 
+        data-name='${name}'
+        data-price='${price}'
+        data-img='${img}'
+        >Add to cart</button>
+    </div>
+  </div>
+  `
+}
+
+const renderPopulars = () => {
+    const popularList = productsData.filter(
+        product => product.popular === true
+    )
+    popularProducts.innerHTML = popularList.map(renderProduct).join('')
+}
+
+const renderDividedProducts = (index = 0) => {
+    products.innerHTML += productsController.dividedProducts[index].map(renderProduct).join('');
+}
+
+const renderFilteredProducts = (category) => {
+    const productsList = productsData.filter(
+        (product) => product.category === category
+    )
+    products.innerHTML = productsList.map(renderProduct).join('')
+}
+
+const renderProducts = (index = 0, category = undefined) =>{
+    if(!category) {
+        renderDividedProducts(index);
+        return;
+    }
+    renderFilteredProducts(category)
+}
+
+const changeCategory = (selectedCategory) => {
+    const categories = [...categoriesList];
+    categories.forEach((categoryBtn) => {
+        if(categoryBtn.dataset.category !== selectedCategory){
+            categoryBtn.classList.remove('categoryActive');
+            return;
+        }
+        categoryBtn.classList.add('categoryActive');
+    })
+}
+
+const toggleMoreBtn = (category) => {
+    if (!category) {
+        moreBtn.classList.remove('hidden');
+        return
+    }
+    moreBtn.classList.add('hidden');
+};
+
+const changeFilterState = (e) => {
+    const selectedCategory = e.target.dataset.category;
+    changeCategory(selectedCategory);
+    toggleMoreBtn(selectedCategory);
+}
+
+const applyFilter = (e) => {
+    if (!e.target.classList.contains('category')) return;
+    changeFilterState(e);
+    if(!e.target.dataset.category){
+        products.innerHTML = '';
+        renderProducts();
+    }else{
+        renderProducts(0, e.target.dataset.category);
+        productsController.nextProductsIndex =1;
+    }
+}
+
+const isLastIndexOf = () => productsController.nextProductsIndex === productsController.productsLimit;
+
+const showMoreProducts =() => {
+    renderProducts(productsController.nextProductsIndex);
+    productsController.nextProductsIndex ++;
+    if (isLastIndexOf()){
+        moreBtn.classList.add('hidden');
+    }
+}
+
+//add products
+
+const showSuccessModal = (msg) => {
+  successModal.classList.add('activeModal');
+  successModal.textContent = msg;
+  setTimeout (() => {
+      successModal.classList.remove('activeModal');
+  }, 1600)
+}
+
+const addProducts = (e) => {
+  if(!e.target.classList.contains('addBtn')) return;
+  const {id, name, price, img} = e.target.dataset;
+  const product = productData (id, name, price, img);
+  if (isExistingProduct(product)) {
+      addUnitToProduct(product);
+      showSuccessModal ("Unit added successfully");
+
+  } else {
+      createCartProduct (product);
+      showSuccessModal ("Product added successfully");
+  }
+  checkCartState ()
+}
+
+
+
+const initproducts = () => {
+  renderProducts()
+  categories.addEventListener("click", applyFilter)
+  moreBtn.addEventListener("click", showMoreProducts)
+  renderPopulars()
+  products.addEventListener("click", addProducts)
+  popularProducts.addEventListener("click", addProducts)
+}
+
+initproducts();
